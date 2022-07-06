08c0e4beb0de3e9b4a8cc2f6b80cd8506dfa8cff
MarcinSlusarczyk <marcink442@gmail.com>
2022-07-06 23:16:49 +0200

    Merge remote-tracking branch 'remote/master'
    # Conflicts:
    #	Shop/Shop/settings.py

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -115,14 +115,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
