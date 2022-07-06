8444918deb7d3dadf0a10c3e0bec889af1d9fe76
MarcinSlusarczyk <marcink442@gmail.com>
2022-07-06 21:15:04 +0200

    Merge remote-tracking branch 'remote/master'
    # Conflicts:
    #	Shop/Shop/settings.py

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://slusarczyk.herokuapp.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
