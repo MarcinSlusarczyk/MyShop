9a361b6d80c3f0a9c082251ce6d22ab7e3118129
MarcinSlusarczyk <marcink442@gmail.com>
2022-07-06 23:08:26 +0200

    Merge remote-tracking branch 'remote/master'
    # Conflicts:
    #	Shop/Shop/settings.py

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
