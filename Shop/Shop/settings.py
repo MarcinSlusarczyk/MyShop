27178c8a32f938f87b92eae99189ed3d7538351d
MarcinSlusarczyk <marcink442@gmail.com>
2022-07-08 15:59:35 +0200

    Merge remote-tracking branch 'remote/master'
    # Conflicts:
    #	Shop/Shop/settings.py

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-i#ewth9s()*$(cr#jp@y@&wr!1-cq791cg$ljclb5)i)*&rdjx'  #os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
